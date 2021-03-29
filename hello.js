aee4bdab34358f5f1e17c83cc77dc46bb9c0852c
nathan pham <nathan@thienky.com>
2021-03-28 17:02:25 -0700

    add 2 more line

--- hello.js
+++ hello.js
@@ -3,3 +3,5 @@ console.log("lolandft");
 console.log("chat hello.js");
 console.log("nathan the best");
 console.log("chicken is good");
+console.log("coding is fun");
+console.log("bedwars is fun")
